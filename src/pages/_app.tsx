f45bae4c47f39d151063fda5c60159b6491bc790
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-04-20 11:25:45 -0300

    Day 2: Component Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/global.scss'
 
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.appWrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,6 @@ import { Header } from "../components/Header";
 
 export default function Home() {
   return (
-    <Header />
+    <h1>Index</h1>
   )
 }
