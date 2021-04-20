69b63761908130183de9995f33b32f4544757291
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-04-20 10:17:48 -0300

    Day 2: Component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR
+  });
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Logo"/>
+
+      <p>O melhor para você ouvir, sempre</p>
+      <span>{currentDate}</span>
+    </header>
+  );
+}
