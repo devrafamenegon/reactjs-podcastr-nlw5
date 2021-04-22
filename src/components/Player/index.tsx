530d5b176ac2b23bcf9221e0bae53cdd5105cc75
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-04-22 13:51:51 -0300

    Day 4: Add audio

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -54,6 +54,13 @@ export function Player() {
           <span>00:00</span>
         </div>
 
+        { episode && (
+          <audio 
+            src={episode.url}
+            autoPlay
+          />
+        )}
+
         <div className={styles.buttons}>
           <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
